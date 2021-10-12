522a04dcb3e47bcc1a3df1d4421edb032c9eeb38
Kai Hirota <kaiyhirota@gmail.com>
2021-10-12 18:54:54 +1100

    fixing KDTree
    - fixed naming issues
    - made fromNodes a factory method
    - moved comparator to node class
    - made KDTreeNode inherit GeoPoint
    - fixed getDistance method to radian

--- src/main/java/com/comp6442/route42/geosearch/KDTree.java
+++ src/main/java/com/comp6442/route42/geosearch/KDTree.java
@@ -1,231 +1,129 @@
 package com.comp6442.route42.geosearch;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * KD_Tree class creates KD-Tree and search nearest points
- * Reference and explanation for KD-Tree is at below
- * https://rosettacode.org/wiki/K-d_tree#Java
+ * KD_Tree class creates KD-Tree and search nearest points Reference and explanation for KD-Tree is
+ * at below https://rosettacode.org/wiki/K-d_tree#Java
  */
-
 public class KDTree {
-    private Node rootNode = null;
-    private Node bestNode = null;
-    private List<Node> kbestNodes = new ArrayList<>();
-    private double bestDistance = 0;
-    private List<Double> kbestDistances = new ArrayList<>();
+  private final List<Double> kbestDistances = new ArrayList<>();
+  private final KDTreeNode rootNode;
+  private KDTreeNode bestNode = null;
+  private List<KDTreeNode> kbestNodes = new ArrayList<>();
+  private double bestDistance = 0;
 
-    public KD_Tree(){
+  public KDTree(KDTreeNode node) {
+    this.rootNode = node;
+  }
 
+  public KDTreeNode findNearest(KDTreeNode target) {
+    if (rootNode == null) {
+      throw new IllegalStateException("Tree is Empty!");
     }
+    bestNode = null;
+    bestDistance = 0;
+    searchNearest(rootNode, target, 0);
+    return bestNode;
+  }
 
-    public KD_Tree(Node node){
-        this.rootNode = node;
+  private void searchNearest(KDTreeNode root, KDTreeNode target, int index) {
+    if (root == null) {
+      return;
     }
-
-    public KDTree(List<Node> nodes){
-        this.rootNode = createTree(nodes, 0, nodes.size(), 0);
+    double d = root.getDistanceTo(target);
+    if (bestNode == null || d < bestDistance) {
+      bestDistance = d;
+      bestNode = root;
     }
-
-    public Node findNearest(Node target){
-        if(rootNode == null){
-            throw new IllegalStateException("Tree is Empty!");
-        }
-        bestNode = null;
-        bestDistance = 0;
-        searchNearest(rootNode, target, 0);
-        return bestNode;
+    if (bestDistance == 0) {
+      return;
     }
+    double diff = root.getCoordValue(index) - target.getCoordValue(index);
+    index = (index + 1) % 2;
+    searchNearest(diff > 0 ? root.getLeft() : root.getRight(), target, index);
+    if (Math.sqrt(diff * diff) >= bestDistance) {
+      return;
+    }
+    searchNearest(diff > 0 ? root.getRight() : root.getLeft(), target, index);
+  }
 
-    private void searchNearest(Node root, Node target, int index){
-        if(root == null){
+  public List<KDTreeNode> findKNearest(int k, double lat, double lon) {
+    KDTreeNode target = new KDTreeNode(lat, lon);
+    if (rootNode == null) {
+      throw new IllegalStateException("Tree is Empty!");
+    }
+    kbestNodes = new ArrayList<>();
+    bestNode = null;
+    bestDistance = 0;
+    int store = 0;
+    while (store < k) {
+      searchKNearest(rootNode, k, store, target, 0);
+      kbestDistances.add(bestDistance);
+      kbestNodes.add(bestNode);
+      bestNode = null;
+      bestDistance = 0;
+      store++;
+    }
+    return kbestNodes;
+  }
+
+  private void searchKNearest(KDTreeNode root, int k, int store, KDTreeNode target, int index) {
+    if (root == null) {
+      return;
+    }
+    double d = root.getDistanceTo(target);
+    if (bestNode == null || d < bestDistance) {
+      if (kbestDistances.size() != 0) {
+        if ((Double) d <= kbestDistances.get(store - 1)) {
+          double diff = root.getCoordValue(index) - target.getCoordValue(index);
+          index = (index + 1) % 2;
+          searchKNearest(diff > 0 ? root.getLeft() : root.getRight(), k, store, target, index);
+          if (Math.sqrt(diff * diff) >= Math.max(bestDistance, kbestDistances.get(store - 1))) {
             return;
+          }
+          searchKNearest(diff > 0 ? root.getRight() : root.getLeft(), k, store, target, index);
+        } else {
+          bestDistance = d;
+          bestNode = root;
         }
-        double d = root.getDistanceFrom(target);
-        if(bestNode == null || d < bestDistance){
-            bestDistance = d;
-            bestNode = root;
-        }
-        if(bestDistance == 0){
-            return;
-        }
-        double diff = root.getCoordValue(index) - target.getCoordValue(index);
-        index = (index + 1) % 2;
-        searchNearest(diff > 0 ? root.left : root.right, target, index);
-        if(Math.sqrt(diff * diff) >= bestDistance){
-            return;
-        }
-        searchNearest(diff > 0 ? root.right : root.left, target, index);
+      } else {
+        bestDistance = d;
+        bestNode = root;
+      }
     }
-
-    public List<Node> findkNearest(int k, double lat, double lon){
-        Node target = new Node(lat, lon);
-        if(rootNode == null){
-            throw new IllegalStateException("Tree is Empty!");
-        }
-        kbestNodes = new ArrayList<>();
-        bestNode = null;
-        bestDistance = 0;
-        int store = 0;
-        while(store < k){
-            searchkNearest(rootNode, k, store, target, 0);
-            kbestDistances.add(bestDistance);
-            kbestNodes.add(bestNode);
-            bestNode = null;
-            bestDistance = 0;
-            store++;
-        }
-        return kbestNodes;
+    if (bestDistance == 0) {
+      return;
     }
-
-    private void searchkNearest(Node root, int k, int store, Node target, int index){
-        if(root == null){
-            return;
-        }
-        double d = root.getDistanceFrom(target);
-        if(bestNode == null || d < bestDistance){
-            if(kbestDistances.size() != 0){
-                if((Double)d <= kbestDistances.get(store-1)){
-                    double diff = root.getCoordValue(index) - target.getCoordValue(index);
-                    index = (index + 1) % 2;
-                    searchkNearest(diff > 0 ? root.left : root.right, k, store, target, index);
-                    if(Math.sqrt(diff * diff) >= Math.max(bestDistance,kbestDistances.get(store-1))){
-                        return;
-                    }
-                    searchkNearest(diff > 0 ? root.right : root.left, k, store, target, index);
-                }
-                else{
-                    bestDistance = d;
-                    bestNode = root;
-                }
-            }
-            else{
-                bestDistance = d;
-                bestNode = root;
-            }
-        }
-        if(bestDistance == 0){
-            return;
-        }
-        double diff = root.getCoordValue(index) - target.getCoordValue(index);
-        index = (index + 1) % 2;
-        searchkNearest(diff > 0 ? root.left : root.right, k, store, target, index);
-        if(Math.sqrt(diff * diff) >= bestDistance){
-            return;
-        }
-        searchkNearest(diff > 0 ? root.right : root.left, k, store, target, index);
+    double diff = root.getCoordValue(index) - target.getCoordValue(index);
+    index = (index + 1) % 2;
+    searchKNearest(diff > 0 ? root.getLeft() : root.getRight(), k, store, target, index);
+    if (Math.sqrt(diff * diff) >= bestDistance) {
+      return;
     }
+    searchKNearest(diff > 0 ? root.getRight() : root.getLeft(), k, store, target, index);
+  }
 
-    public double getBestDistance() {
-        return bestDistance;
-    }
+  public double getBestDistance() {
+    return bestDistance;
+  }
 
-    public double getkBestDistance(int index) {
-        return kbestDistances.get(index);
-    }
+  public double getKBestDistance(int index) {
+    return kbestDistances.get(index);
+  }
 
-    private Node createTree(List<Node> nodes, int begin, int end, int index){
-        if(end <= begin){
-            return null;
-        }
-        int n = begin + (end - begin)/2;
-        Node node = QuickSelect.select(nodes, begin, end - 1, n, new NodeComparator(index));
-        index = (index + 1) % 2;
-        node.left = createTree(nodes, begin, n, index);
-        node.right = createTree(nodes, n+1, end, index);
-        return node;
-    }
+  public static KDTree fromNodes(List<KDTreeNode> nodes) {
+    if (nodes.size() == 0) return null;
+    KDTreeNode node = KDTreeNode.fromNodes(nodes, 0, nodes.size(), 0);
+    return new KDTree(node);
+  }
 
-    public String toString(){
-        StringBuilder SB = new StringBuilder();
-        SB.append(this.rootNode);
-        return String.valueOf(SB);
-    }
+  public String toString() {
+    return this.rootNode.toString();
+  }
 
-    public String display(int tabs){
-        StringBuilder SB = new StringBuilder();
-        SB.append(this.rootNode.display(tabs));
-        return String.valueOf(SB);
-    }
-
-    private static class NodeComparator implements Comparator<Node> {
-        private int index;
-        private NodeComparator(int index){
-            this.index = index;
-        }
-
-        @Override
-        public int compare(Node node1, Node node2) {
-            return Double.compare(node1.getCoordValue(index),node2.getCoordValue(index));
-        }
-    }
-
-    public static class Node{
-        private double[] coords = new double[2];
-        private double lat, lon;
-        private Node left = null;
-        private Node right = null;
-
-        public Node(double[] coords){
-            this.coords = coords;
-            this.lat = coords[0];
-            this.lon = coords[1];
-        }
-
-        public Node(double latitude, double longitude){
-            this.coords[0] = latitude;
-            this.lat = latitude;
-            this.coords[1] = longitude;
-            this.lon = longitude;
-        }
-
-        double[] getCoords(){
-            return this.coords;
-        }
-
-        double getLatitude() {
-            return this.lat;
-        }
-
-        double getLongitude() {
-            return this.lon;
-        }
-
-        double getCoordValue(int index){
-            return this.getCoords()[index];
-        }
-
-        double getDistanceFrom(Node node){
-            double dist = 0;
-            for(int i=0; i < this.coords.length; i++){
-                double d_each = this.coords[i] - node.coords[i];
-                dist += d_each * d_each;
-            }
-            return Math.sqrt(dist);
-        }
-
-        public String toString(){
-            StringBuilder SB = new StringBuilder();
-            SB.append("(");
-            for(int i=0; i < this.coords.length; i++){
-                if(i > 0)
-                    SB.append(", ");
-                SB.append(this.coords[i]);
-            }
-            SB.append(")");
-            return String.valueOf(SB);
-        }
-
-        public String display(int tabs) {
-            StringBuilder SB = new StringBuilder();
-            SB.append(this);
-            if(this.left != null){
-                SB.append("\n").append("\t".repeat(tabs)).append("├─").append(this.left.display(tabs + 1));
-            }
-            if(this.right != null) {
-                SB.append("\n").append("\t".repeat(tabs)).append("├─").append(this.right.display(tabs + 1));
-            }
-            return SB.toString();
-        }
-    }
+  public String display(int tabs) {
+    return this.rootNode.display(tabs);
+  }
 }
--- src/test/java/com/comp6442/route42/geosearch/KDTreeTest.java
+++ src/test/java/com/comp6442/route42/geosearch/KDTreeTest.java
@@ -1,121 +1,116 @@
 package com.comp6442.route42.geosearch;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
 import java.util.Random;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 
 public class KDTreeTest {
 
     @Test
     public void testFindOneNearest() {
 
-        List<KD_Tree.Node> nodes = generateNodes(3);
-        KD_Tree tree = generateKD_Tree(nodes);
+        List<KDTreeNode> nodes = generateNodes(3);
+        KDTree tree = generateKDTree(nodes);
         System.out.println(tree.display(0));
 
-        KD_Tree.Node target = generateTarget();
+        KDTreeNode target = generateTarget();
         System.out.println(" target node : " + target);
         int i = 0;
-        for(KD_Tree.Node node : nodes){
-            System.out.println(" " + i + "-th node : " + node + " distance : " + node.getDistanceFrom(target));
+        for(KDTreeNode node : nodes){
+            System.out.println(" " + i + "-th node : " + node + " distance : " + node.getDistanceTo(target));
             i++;
         }
 
-        KD_Tree.Node nearest_node = tree.findNearest(target.getLatitude(),target.getLongitude());
+        KDTreeNode nearest_node = tree.findNearest(target);
         System.out.println(" nearest neighbor node : " + nearest_node + " distance : " + tree.getBestDistance());
 
-        assertEquals("Wrong Coordinate", nearest_node.getDistanceFrom(target), tree.getBestDistance(), 1e-8);
+        Assertions.assertEquals(nearest_node.getDistanceTo(target), tree.getBestDistance(), 1e-8, "Wrong Coordinate");
 
         nodes = generateNodes(7);
-        tree = generateKD_Tree(nodes);
+        tree = generateKDTree(nodes);
         System.out.println(tree.display(0));
 
         target = generateTarget();
         System.out.println(" target node : " + target);
         i = 0;
-        for(KD_Tree.Node node : nodes){
-            System.out.println(" " + i + "-th node : " + node + " distance : " + node.getDistanceFrom(target));
+        for(KDTreeNode node : nodes){
+            System.out.println(" " + i + "-th node : " + node + " distance : " + node.getDistanceTo(target));
             i++;
         }
 
-        nearest_node = tree.findNearest(target.getLatitude(),target.getLongitude());
+        nearest_node = tree.findNearest(target);
         System.out.println(" nearest neighbor node : " + nearest_node + " distance : " + tree.getBestDistance());
 
-        assertEquals("Wrong Coordinate", nearest_node.getDistanceFrom(target), tree.getBestDistance(), 1e-8);
+        Assertions.assertEquals(nearest_node.getDistanceTo(target), tree.getBestDistance(), 1e-8, "Wrong Coordinate");
     }
 
     @Test
     public void testFindMultiNearest() {
         int k_points = 10;
-        List<KD_Tree.Node> nodes = generateNodes(50);
-        KD_Tree tree = generateKD_Tree(nodes);
+        List<KDTreeNode> nodes = generateNodes(50);
+        KDTree tree = generateKDTree(nodes);
         System.out.println(tree.display(0));
 
-        KD_Tree.Node target = generateTarget();
+        KDTreeNode target = generateTarget();
         int i = 0;
         System.out.println(" target node : " + target);
-        for(KD_Tree.Node node : nodes){
-            System.out.println(" " + i + "-th node : " + node + " distance : " + node.getDistanceFrom(target));
+        for(KDTreeNode node : nodes){
+            System.out.println(" " + i + "-th node : " + node + " distance : " + node.getDistanceTo(target));
             i++;
         }
 
-        List<KD_Tree.Node> nearest_nodes = tree.findkNearest(k_points, target.getLatitude(),target.getLongitude());
+        List<KDTreeNode> nearest_nodes = tree.findKNearest(k_points, target.getLatitude(),target.getLongitude());
         i = 0;
-        for(KD_Tree.Node nearest_node : nearest_nodes){
-            System.out.println(" " + i + "-th nearest neighbor node : " + nearest_node + " distance : " + tree.getkBestDistance(i));
-            assertEquals("Wrong Coordinate", nearest_node.getDistanceFrom(target), tree.getkBestDistance(i), 1e-8);
+        for(KDTreeNode nearest_node : nearest_nodes){
+            System.out.println(" " + i + "-th nearest neighbor node : " + nearest_node + " distance : " + tree.getKBestDistance(i));
+            Assertions.assertEquals(nearest_node.getDistanceTo(target), tree.getKBestDistance(i), 1e-8, "Wrong Coordinate");
             i++;
         }
 
         nodes = generateNodes(100);
-        tree = generateKD_Tree(nodes);
+        tree = generateKDTree(nodes);
         System.out.println(tree.display(0));
 
         target = generateTarget();
         System.out.println(" target node : " + target);
         i = 0;
-        for(KD_Tree.Node node : nodes){
-            System.out.println(" " + i + "-th node : " + node + " distance : " + node.getDistanceFrom(target));
+        for(KDTreeNode node : nodes){
+            System.out.println(" " + i + "-th node : " + node + " distance : " + node.getDistanceTo(target));
             i++;
         }
 
-        nearest_nodes = tree.findkNearest(k_points, target.getLatitude(),target.getLongitude());
+        nearest_nodes = tree.findKNearest(k_points, target.getLatitude(),target.getLongitude());
         i = 0;
-        for(KD_Tree.Node nearest_node : nearest_nodes){
-            System.out.println(" " + i + "-th nearest neighbor node : " + nearest_node + " distance : " + tree.getkBestDistance(i));
-            assertEquals("Wrong Coordinate", nearest_node.getDistanceFrom(target), tree.getkBestDistance(i), 1e-8);
+        for(KDTreeNode nearest_node : nearest_nodes){
+            System.out.println(" " + i + "-th nearest neighbor node : " + nearest_node + " distance : " + tree.getKBestDistance(i));
+            Assertions.assertEquals(nearest_node.getDistanceTo(target), tree.getKBestDistance(i), 1e-8, "Wrong Coordinate");
             i++;
         }
     }
 
-    public static List<KD_Tree.Node> generateNodes(int points) {
+    public static List<KDTreeNode> generateNodes(int points) {
         Random random = new Random();
-        List<KD_Tree.Node> nodes = new ArrayList<>();
+        List<KDTreeNode> nodes = new ArrayList<>();
         for(int i=0; i < points; i++){
             nodes.add(randomNPoint(random));
         }
         return nodes;
     }
 
-    public static KD_Tree generateKD_Tree(List<KD_Tree.Node> nodes) {
-        KD_Tree tree = new KD_Tree(nodes);
+    public static KDTree generateKDTree(List<KDTreeNode> nodes) {
+        KDTree tree = KDTree.fromNodes(nodes);;
         return tree;
     }
 
-    public static KD_Tree.Node generateTarget() {
+    public static KDTreeNode generateTarget() {
         Random random = new Random();
-        KD_Tree.Node nodes = randomNPoint(random);
+        KDTreeNode nodes = randomNPoint(random);
         return nodes;
     }
 
-    private static KD_Tree.Node randomNPoint(Random random){
-        double[] coord = new double[2];
-        coord[0] = random.nextDouble();
-        coord[1] = random.nextDouble();
-        return new KD_Tree.Node(coord);
+    private static KDTreeNode randomNPoint(Random random){
+        return new KDTreeNode(random.nextDouble(), random.nextDouble());
     }
 }
