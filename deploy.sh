dc09601e807709fa45b3c3e8bc653dbd6fea5599
Kai Hirota <kaiyhirota@gmail.com>
2021-10-12 20:10:23 +1100

    integrate KDTree

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 KEY="route42.pem"
-CREDS="-firebase-adminsdk-5s99m-cb8b011821.json"
+CREDS="google_application_credentials_route42.json"
 AWS_USERNAME="ec2-user"
 AWS_HOSTNAME="ec2-13-211-169-204.ap-southeast-2.compute.amazonaws.com"
 
@@ -17,8 +17,8 @@ echo "Copied jar file from local machine to ec2 instance"
 scp -i $KEY execute_commands_ec2.sh ${AWS_USERNAME}@${AWS_HOSTNAME}:/home/ec2-user
 echo "Copied latest startup script from local machine to ec2 instance"
 
-scp -i $KEY "$CREDS" ${AWS_USERNAME}@${AWS_HOSTNAME}:${HOME}/google_application_credentials_route42.json
+scp -i $KEY $CREDS ${AWS_USERNAME}@${AWS_HOSTNAME}:/home/ec2-user/google_application_credentials_route42.json
 echo "Copied google credentials from local machine to ec2 instance"
 
 echo "Connecting to ec2 instance and starting server using java -jar command"
-ssh -i "route42.pem" ec2-user@ec2-13-211-169-204.ap-southeast-2.compute.amazonaws.com ./execute_commands_ec2.sh
+ssh -i $KEY ec2-user@${AWS_HOSTNAME} ./execute_commands_ec2.sh
--- execute_commands_ec2.sh
+++ execute_commands_ec2.sh
@@ -3,5 +3,5 @@
 kill $(lsof -t -i)
 echo "Killed process running"
 
-GOOGLE_APPLICATION_CREDENTIALS=google_application_credentials_route42.json nohup java -jar Route42RestAPI-1.0-SNAPSHOT.jar &
+GOOGLE_APPLICATION_CREDENTIALS=./google_application_credentials_route42.json nohup java -jar Route42RestAPI-1.0-SNAPSHOT.jar &
 echo "Started server using java -jar command"
